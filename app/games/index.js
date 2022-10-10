054e4a290682214928025889b90209460a77e320
Nicolas Bruno Morais <nicolasbrunomorais@hotmail.com>
2022-10-09 21:36:54 -0300

    First commit

---
+++ app/games/index.js
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+import { addDays, formatISO } from "date-fns";
+const prisma = new PrismaClient();
+
+export const listGames = async (ctx) => {
+  const currentDate = ctx.request.query.gameTime;
+  const where = currentDate ? {
+        gameTime: {
+          gte: currentDate,
+          lt: formatISO(addDays(new Date(currentDate), 1)),
+        },
+      }
+    : {};
+
+  try {
+    const games = await prisma.game.findMany({
+      where,
+    });
+    ctx.body = games;
+    ctx.status = 200;
+  } catch (error) {
+    ctx.body = error;
+    ctx.status = 400;
+    console.log(error);
+  }
+};
